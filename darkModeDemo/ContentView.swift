09f674df7f02f6e63576d01c2437e3eb0ca071d6
Logan Koshenka <koshenka27@gmail.com>
2021-05-10 22:48:47 -0400

    finished

--- darkModeDemo/ContentView.swift
+++ darkModeDemo/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color("sky")
+            
+            Circle()
+                .frame(width: 300)
+                .offset(x: 150, y: -350)
+                .foregroundColor(Color(colorScheme == .light ? #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1) : #colorLiteral(red: 0, green: 1, blue: 0.04290095224, alpha: 1)))
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
     }
 }
