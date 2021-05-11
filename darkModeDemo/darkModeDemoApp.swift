88860e2f2ec9a0c0c66fba39b0d407bee7994d25
Logan Koshenka <koshenka27@gmail.com>
2021-05-10 22:14:20 -0400

    Initial Commit

---
+++ darkModeDemo/darkModeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  darkModeDemoApp.swift
+//  darkModeDemo
+//
+//  Created by Logan Koshenka on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct darkModeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
